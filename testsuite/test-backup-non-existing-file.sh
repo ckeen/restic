849a6b8391bd9a2bd135632c9ff31ff00b699836
Christian Kellermann <ckeen@pestilenz.org>
2015-05-05 20:18:46 +0200

    Remove timeout command

--- testsuite/test-backup-non-existing-file.sh
+++ testsuite/test-backup-non-existing-file.sh
@@ -5,6 +5,6 @@ prepare
 run restic init
 
 # start backup with non existing dir
-run timeout 10s restic.debug backup "${BASE}/fake-data/0/0/"{0,1,foobar,5} && debug "done" || false
+run restic.debug backup "${BASE}/fake-data/0/0/"{0,1,foobar,5} && debug "done" || false
 
 cleanup
